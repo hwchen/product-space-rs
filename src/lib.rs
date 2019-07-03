81f7c3f14a2027e4c41fb35f32e03580bdb096a4
Walther Chen <walther.chen@gmail.com>
2019-07-03 00:35:49 -0400

    begin ProductSpace struct, rca with years window

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use nalgebra::DMatrix;
+use std::collections::HashMap;
+
 mod rca;
 pub use rca::{
     apply_fair_share,
@@ -17,3 +20,43 @@ pub use distance::distance;
 
 mod complexity;
 pub use complexity::complexity;
+
+// Currently just country and product.
+// May make this more general in the future
+pub struct ProductSpace {
+    country_index: Vec<String>,
+    product_index: Vec<String>,
+    mcps: HashMap<u32, DMatrix<f64>>,
+}
+
+impl ProductSpace {
+    // TODO Result and error handling for year range mistakes
+    pub fn rca(
+        &self,
+        _cutoff: f64,
+        years: &[u32],
+        ) -> DMatrix<f64>
+    {
+        let year_count = years.len();
+
+        if year_count > 1 {
+            let zeros = DMatrix::zeros(self.country_index.len(), self.product_index.len());
+            let agg_mcp = years.iter()
+                // in future, should return error if
+                // year not present
+                .filter_map(|y| self.mcps.get(&y))
+                .fold(zeros, |sum, x| sum + x);
+
+            rca(&agg_mcp)
+        } else if year_count == 1 {
+            // no extra allocation for mcp
+
+            let y = years[0];
+            let mcp = self.mcps.get(&y).expect("get rid of this panic");
+
+            rca(&mcp)
+        } else {
+            DMatrix::zeros(1,1)
+        }
+    }
+}
